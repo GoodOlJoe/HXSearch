722ed845b0522894fd3abfb00a90ef078d70cf8f
Joe Eggers <jeggers@jackel.net>
2024-12-30 22:18:26 -0500

    complete linear traversal

---
+++ HXSearch/TraversalHandlers/LinearPathSignature.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using HXSearch.Models;
+using QuikGraph;
+
+
+namespace HXSearch.TraversalHandlers
+{
+    internal class LinearPathSignature
+    {
+        internal IEnumerable<string> Paths => _paths;
+        private readonly StringBuilder sb = new(50);
+        private readonly List<string> _paths = new(10);
+
+        internal void Subscribe(Preset preset) => preset.OnPostLinearPath += PostLinearPathHandler;
+        internal void UnSubscribe(Preset preset) => preset.OnPostLinearPath -= PostLinearPathHandler;
+
+        internal void PostLinearPathHandler(AdjacencyGraph<Node, Edge<Node>> graph, Preset preset, List<Node> path)
+        {
+            sb.Clear();
+            foreach (Node n in path.Where(n =>
+                null != n.Model &&
+                n.Model.Category != ModelCategory.Split &&
+                n.Model.Category != ModelCategory.Merge &&
+                n.Model.Category != ModelCategory.Dummy))
+            {
+                sb.Append(n.Model.Signature);
+            }
+            _paths.Add(sb.ToString());
+        }
+    }
+}
